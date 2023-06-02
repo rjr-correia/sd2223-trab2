4d248c6c165eb69783847e1fd43fc4fc0125e9bb
rjr-correia <rjr.correia@campus.fct.unl.pt>
2023-06-02 20:28:23 +0100

    Changes

--- src/sd2223/trab1/servers/rest/RestUsersServer.java
+++ src/sd2223/trab1/servers/rest/RestUsersServer.java
@@ -1,17 +1,24 @@
 package sd2223.trab1.servers.rest;
 
+import java.net.InetAddress;
+import java.net.URI;
 import java.util.logging.Logger;
 
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import sd2223.trab1.api.java.Users;
 import sd2223.trab1.servers.Domain;
 import utils.Args;
 
+import javax.net.ssl.SSLContext;
+
 
 public class RestUsersServer extends AbstractRestServer {
 	public static final int PORT = 3456;
-	
+	static final String SERVER_URI_FMT = "https://%s:%s/rest";
+
+
 	private static Logger Log = Logger.getLogger(RestUsersServer.class.getName());
 
 	RestUsersServer() {
@@ -31,6 +38,12 @@ public class RestUsersServer extends AbstractRestServer {
 
 		Args.use(args);
 		Domain.set( args[0], Long.valueOf(args[1]));
-		new RestUsersServer().start();
+		//new RestUsersServer().start();
+		var config = new ResourceConfig();
+		config.register(RestUsersResource.class);
+
+		var ip = InetAddress.getLocalHost().getHostAddress();
+		var serverURI = URI.create(String.format(SERVER_URI_FMT, ip, PORT));
+		JdkHttpServerFactory.createHttpServer( serverURI, config, SSLContext.getDefault());
 	}	
 }
--- src/sd2223/trab1/servers/soap/SoapUsersServer.java
+++ src/sd2223/trab1/servers/soap/SoapUsersServer.java
@@ -1,11 +1,19 @@
 package sd2223.trab1.servers.soap;
 
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsServer;
+import jakarta.xml.ws.Endpoint;
 import sd2223.trab1.api.java.Users;
 import sd2223.trab1.servers.Domain;
+
+import javax.net.ssl.SSLContext;
 
 public class SoapUsersServer extends AbstractSoapServer<SoapUsersWebService> {
 
@@ -19,6 +27,18 @@ public class SoapUsersServer extends AbstractSoapServer<SoapUsersWebService> {
 	public static void main(String[] args) throws Exception {		
 		Domain.set( args[0], 0);
 		Log.setLevel(Level.INFO);
-		new SoapUsersServer().start();
+		//new SoapUsersServer().start();
+
+		var ip = InetAddress.getLocalHost().getHostAddress();
+
+		var server = HttpsServer.create(new InetSocketAddress(ip, PORT), 0);
+
+		server.setExecutor(Executors.newCachedThreadPool());
+		server.setHttpsConfigurator(new HttpsConfigurator(SSLContext.getDefault()));
+
+		var endpoint = Endpoint.create(new SoapUsersWebService());
+		endpoint.publish(server.createContext("/soap"));
+
+		server.start();
 	}
 }
