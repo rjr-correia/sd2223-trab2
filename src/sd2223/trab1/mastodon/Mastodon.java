320bef6eeb17c4c52d72d7cedaefd5c4ac00cb77
Guilherme <gni.fernandes@campus.fct.unl.pt>
2023-06-03 11:34:54 +0100

    changes

--- src/sd2223/trab1/mastodon/Mastodon.java
+++ src/sd2223/trab1/mastodon/Mastodon.java
@@ -27,148 +27,257 @@ import sd2223.trab1.servers.Domain;
 import sd2223.trab1.servers.soap.SoapUsersServer;
 import utils.JSON;
 
-public class Mastodon implements Feeds{
-	
-	static String MASTODON_NOVA_SERVER_URI = "http://10.170.138.52:3000";
-	static String MASTODON_SOCIAL_SERVER_URI = "https://mastodon.social";
-	
-	static String MASTODON_SERVER_URI = MASTODON_NOVA_SERVER_URI;
-	
-	private static final String clientKey = "hKcoCjq-Cjfgrs8E5GEBv2TdZSTU4acLrJL6JPepQlw";
-	private static final String clientSecret = "28-azi3iCJNCo6Vy7AHYFwdXUsw363VoyL8Dcfk9Od0";
-	private static final String accessTokenStr = "ztBowRMxYi5VR51vPlfj_ABMltXSOvuf_mFJw9Sx2KE";
+public class Mastodon implements Feeds {
 
-	static final String STATUSES_PATH= "/api/v1/statuses";
-	static final String TIMELINES_PATH = "/api/v1/timelines/home";
-	static final String ACCOUNT_FOLLOWING_PATH = "/api/v1/accounts/%s/following";
-	static final String VERIFY_CREDENTIALS_PATH = "/api/v1/accounts/verify_credentials";
-	static final String SEARCH_ACCOUNTS_PATH = "/api/v1/accounts/search";
-	static final String ACCOUNT_FOLLOW_PATH = "/api/v1/accounts/%s/follow";
-	static final String ACCOUNT_UNFOLLOW_PATH = "/api/v1/accounts/%s/unfollow";
+    static String MASTODON_NOVA_SERVER_URI = "http://10.170.138.52:3000";
+    static String MASTODON_SOCIAL_SERVER_URI = "https://mastodon.social";
 
+    static String MASTODON_SERVER_URI = MASTODON_NOVA_SERVER_URI;
 
+    private static final String clientKey = "hKcoCjq-Cjfgrs8E5GEBv2TdZSTU4acLrJL6JPepQlw";
+    private static final String clientSecret = "28-azi3iCJNCo6Vy7AHYFwdXUsw363VoyL8Dcfk9Od0";
+    private static final String accessTokenStr = "ztBowRMxYi5VR51vPlfj_ABMltXSOvuf_mFJw9Sx2KE";
 
-	private static final int HTTP_OK = 200;
-	private static final int HTTP_NOT_FOUND = 404;
-	private static final int HTTP_BAD_REQUEST = 400;
+    static final String STATUSES_PATH = "/api/v1/statuses";
+    static final String TIMELINES_PATH = "/api/v1/timelines/home";
+    static final String ACCOUNT_FOLLOWING_PATH = "/api/v1/accounts/%s/following";
+    static final String VERIFY_CREDENTIALS_PATH = "/api/v1/accounts/verify_credentials";
+    static final String SEARCH_ACCOUNTS_PATH = "/api/v1/accounts/search";
+    static final String ACCOUNT_FOLLOW_PATH = "/api/v1/accounts/%s/follow";
+    static final String ACCOUNT_UNFOLLOW_PATH = "/api/v1/accounts/%s/unfollow";
 
 
-	protected OAuth20Service service;
-	protected OAuth2AccessToken accessToken;
-
-	private static Mastodon impl;
-
-	private static Logger Log = Logger.getLogger(Mastodon.class.getName());
+    private static final int HTTP_OK = 200;
+    private static final int HTTP_NOT_FOUND = 404;
+    private static final int HTTP_BAD_REQUEST = 400;
 
 
+    protected OAuth20Service service;
+    protected OAuth2AccessToken accessToken;
 
-	protected Mastodon() {
-		try {
-			service = new ServiceBuilder(clientKey).apiSecret(clientSecret).build(MastodonApi.instance());
-			accessToken = new OAuth2AccessToken(accessTokenStr);
-		} catch (Exception x) {
-			x.printStackTrace();
-			System.exit(0);
-		}
-	}
+    private static Mastodon impl;
 
-	synchronized public static Mastodon getInstance() {
-		if (impl == null)
-			impl = new Mastodon();
-		return impl;
-	}
+    private static Logger Log = Logger.getLogger(Mastodon.class.getName());
 
-	private String getEndpoint(String path, Object ... args ) {
-		var fmt = MASTODON_SERVER_URI + path;
-		return String.format(fmt, args);
-	}
-	
-	@Override
-	public Result<Long> postMessage(String user, String pwd, Message msg) {
-		try {
-			final OAuthRequest request = new OAuthRequest(Verb.POST, getEndpoint(STATUSES_PATH));
 
-			JSON.toMap( new PostStatusArgs(msg.getText())).forEach( (k, v) -> {
-				request.addBodyParameter(k, v.toString());
-			});
-			
-			service.signRequest(accessToken, request);
+    protected Mastodon() {
+        try {
+            service = new ServiceBuilder(clientKey).apiSecret(clientSecret).build(MastodonApi.instance());
+            accessToken = new OAuth2AccessToken(accessTokenStr);
+        } catch (Exception x) {
+            x.printStackTrace();
+            System.exit(0);
+        }
+    }
 
-			Response response = service.execute(request);
-			if (response.getCode() == HTTP_OK) {
-				var res = JSON.decode(response.getBody(), PostStatusResult.class);
-				return ok(res.getId());
-			}
-			if (response.getCode() == HTTP_NOT_FOUND) {
-				return error(NOT_FOUND);
-			}
-			if (response.getCode() == HTTP_BAD_REQUEST) {
-				return error(BAD_REQUEST);
-			}
+    synchronized public static Mastodon getInstance() {
+        if (impl == null)
+            impl = new Mastodon();
+        return impl;
+    }
 
-		} catch (Exception x) {
-			x.printStackTrace();
-		}
-		return error(INTERNAL_ERROR);
-	}
+    private String getEndpoint(String path, Object... args) {
+        var fmt = MASTODON_SERVER_URI + path;
+        return String.format(fmt, args);
+    }
 
-	@Override
-	public Result<List<Message>> getMessages(String user, long time) {
-		try {
-			final OAuthRequest request = new OAuthRequest(Verb.GET, getEndpoint(TIMELINES_PATH));
+    @Override
+    public Result<Long> postMessage(String user, String pwd, Message msg) {
+        try {
+            final OAuthRequest request = new OAuthRequest(Verb.POST, getEndpoint(STATUSES_PATH));
 
-			service.signRequest(accessToken, request);
+            JSON.toMap(new PostStatusArgs(msg.getText())).forEach((k, v) -> {
+                request.addBodyParameter(k, v.toString());
+            });
+            service.signRequest(accessToken, request);
 
-			Response response = service.execute(request);
+            Response response = service.execute(request);
+            if (response.getCode() == HTTP_OK) {
+                var res = JSON.decode(response.getBody(), PostStatusResult.class);
+                return ok(res.getId());
+            }
+            if (response.getCode() == HTTP_NOT_FOUND) {
+                return error(NOT_FOUND);
+            }
+            if (response.getCode() == HTTP_BAD_REQUEST) {
+                return error(BAD_REQUEST);
+            }
 
-			if (response.getCode() == HTTP_OK) {
-				List<PostStatusResult> res = JSON.decode(response.getBody(), new TypeToken<List<PostStatusResult>>() {
-				});
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+        return error(INTERNAL_ERROR);
+    }
 
-				return ok(res.stream().map(PostStatusResult::toMessage).toList());
-			}
+    @Override
+    public Result<List<Message>> getMessages(String user, long time) {
+        try {
+            final OAuthRequest request = new OAuthRequest(Verb.GET, getEndpoint(TIMELINES_PATH));
 
-			if (response.getCode() == HTTP_NOT_FOUND) {
-				return error(NOT_FOUND);
-			}
-			if (response.getCode() == HTTP_BAD_REQUEST) {
-				return error(BAD_REQUEST);
-			}
+            service.signRequest(accessToken, request);
 
-		} catch (Exception x) {
-			x.printStackTrace();
-		}
-		return error(Result.ErrorCode.INTERNAL_ERROR);
-	}
+            Response response = service.execute(request);
 
-	
-	@Override
-	public Result<Void> removeFromPersonalFeed(String user, long mid, String pwd) {
-		return error(NOT_IMPLEMENTED);
-	}
+            if (response.getCode() == HTTP_OK) {
+                List<PostStatusResult> res = JSON.decode(response.getBody(), new TypeToken<List<PostStatusResult>>() {
+                });
 
-	@Override
-	public Result<Message> getMessage(String user, long mid) {
-		return error(NOT_IMPLEMENTED);
-	}
+                return ok(res.stream().map(PostStatusResult::toMessage).toList());
+            }
 
-	@Override
-	public Result<Void> subUser(String user, String userSub, String pwd) {
-		return error(NOT_IMPLEMENTED);
-	}
+            if (response.getCode() == HTTP_NOT_FOUND) {
+                return error(NOT_FOUND);
+            }
+            if (response.getCode() == HTTP_BAD_REQUEST) {
+                return error(BAD_REQUEST);
+            }
 
-	@Override
-	public Result<Void> unsubscribeUser(String user, String userSub, String pwd) {
-		return error(NOT_IMPLEMENTED);
-	}
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+        return error(Result.ErrorCode.INTERNAL_ERROR);
+    }
 
-	@Override
-	public Result<List<String>> listSubs(String user) {
-		return error(NOT_IMPLEMENTED);
-	}
 
-	@Override
-	public Result<Void> deleteUserFeed(String user) {
-		return error(NOT_IMPLEMENTED);
-	}
+    @Override
+    public Result<Void> removeFromPersonalFeed(String user, long mid, String pwd) {
+
+        try {
+            final OAuthRequest request = new OAuthRequest(Verb.DELETE, getEndpoint(STATUSES_PATH + "/" + mid));
+
+            service.signRequest(accessToken, request);
+
+            Response response = service.execute(request);
+
+            if (response.getCode() == HTTP_OK) {
+                return ok();
+            }
+
+            if (response.getCode() == HTTP_NOT_FOUND) {
+                return error(NOT_FOUND);
+            }
+            if (response.getCode() == HTTP_BAD_REQUEST) {
+                return error(BAD_REQUEST);
+            }
+
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+        return error(Result.ErrorCode.INTERNAL_ERROR);
+    }
+
+
+    @Override
+    public Result<Message> getMessage(String user, long mid) {
+        try {
+            final OAuthRequest request = new OAuthRequest(Verb.GET, getEndpoint(TIMELINES_PATH + "/" + mid));
+
+            service.signRequest(accessToken, request);
+
+            Response response = service.execute(request);
+
+            if (response.getCode() == HTTP_OK) {
+                PostStatusResult res = JSON.decode(response.getBody(), new TypeToken<PostStatusResult>() {
+                });
+
+                return ok();
+            }
+
+            if (response.getCode() == HTTP_NOT_FOUND) {
+                return error(NOT_FOUND);
+            }
+            if (response.getCode() == HTTP_BAD_REQUEST) {
+                return error(BAD_REQUEST);
+            }
+
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+        return error(Result.ErrorCode.INTERNAL_ERROR);
+    }
+
+    @Override
+    public Result<Void> subUser(String user, String userSub, String pwd) {
+        try {
+            final OAuthRequest request = new OAuthRequest(Verb.POST, getEndpoint(ACCOUNT_FOLLOW_PATH, user));
+
+            service.signRequest(accessToken, request);
+
+            Response response = service.execute(request);
+            if (response.getCode() == HTTP_OK) {
+
+                return ok();
+            }
+            if (response.getCode() == HTTP_NOT_FOUND) {
+                return error(NOT_FOUND);
+            }
+            if (response.getCode() == HTTP_BAD_REQUEST) {
+                return error(BAD_REQUEST);
+            }
+
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+        return error(INTERNAL_ERROR);
+    }
+
+    @Override
+    public Result<Void> unsubscribeUser(String user, String userSub, String pwd) {
+
+        try {
+            final OAuthRequest request = new OAuthRequest(Verb.POST, getEndpoint(ACCOUNT_UNFOLLOW_PATH, user));
+
+            service.signRequest(accessToken, request);
+
+            Response response = service.execute(request);
+            if (response.getCode() == HTTP_OK) {
+
+                return ok();
+            }
+            if (response.getCode() == HTTP_NOT_FOUND) {
+                return error(NOT_FOUND);
+            }
+            if (response.getCode() == HTTP_BAD_REQUEST) {
+                return error(BAD_REQUEST);
+            }
+
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+        return error(INTERNAL_ERROR);
+    }
+
+    @Override
+    public Result<List<String>> listSubs(String user) {
+        try {
+            final OAuthRequest request = new OAuthRequest(Verb.GET, getEndpoint(TIMELINES_PATH));
+
+            service.signRequest(accessToken, request);
+
+            Response response = service.execute(request);
+
+            if (response.getCode() == HTTP_OK) {
+                List<PostStatusResult> res = JSON.decode(response.getBody(), new TypeToken<List<PostStatusResult>>() {
+                });
+
+                return ok(res.stream().map(PostStatusResult::toMessage).toList());
+            }
+
+            if (response.getCode() == HTTP_NOT_FOUND) {
+                return error(NOT_FOUND);
+            }
+            if (response.getCode() == HTTP_BAD_REQUEST) {
+                return error(BAD_REQUEST);
+            }
+
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+        return error(Result.ErrorCode.INTERNAL_ERROR);
+    }
+
+    @Override
+    public Result<Void> deleteUserFeed(String user) {
+        return error(NOT_IMPLEMENTED);
+    }
 }
