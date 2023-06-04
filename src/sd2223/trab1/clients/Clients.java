dae8817bbdf2d10829b306ff9efe49498265ac35
Guilherme <gni.fernandes@campus.fct.unl.pt>
2023-06-05 00:22:41 +0100

    Changes by rui_j

--- src/sd2223/trab1/clients/Clients.java
+++ src/sd2223/trab1/clients/Clients.java
@@ -1,13 +1,7 @@
 package sd2223.trab1.clients;
 
-import sd2223.trab1.api.java.Feeds;
-import sd2223.trab1.api.java.FeedsPull;
-import sd2223.trab1.api.java.FeedsPush;
-import sd2223.trab1.api.java.Users;
-import sd2223.trab1.clients.rest.RestFeedsClient;
-import sd2223.trab1.clients.rest.RestFeedsPullClient;
-import sd2223.trab1.clients.rest.RestFeedsPushClient;
-import sd2223.trab1.clients.rest.RestUsersClient;
+import sd2223.trab1.api.java.*;
+import sd2223.trab1.clients.rest.*;
 import sd2223.trab1.clients.soap.SoapFeedsClient;
 import sd2223.trab1.clients.soap.SoapFeedsPullClient;
 import sd2223.trab1.clients.soap.SoapFeedsPushClient;
@@ -20,5 +14,8 @@ public class Clients {
 
 	public static final ClientFactory<FeedsPush> FeedsPushClients = new ClientFactory<>(Feeds.SERVICENAME, RestFeedsPushClient::new, SoapFeedsPushClient::new);	
 	
-	public static final ClientFactory<Feeds> FeedsClients = new ClientFactory<>(Feeds.SERVICENAME, RestFeedsClient::new, SoapFeedsClient::new) ; 
+	public static final ClientFactory<Feeds> FeedsClients = new ClientFactory<>(Feeds.SERVICENAME, RestFeedsClient::new, SoapFeedsClient::new) ;
+
+	public static final ClientFactory<FeedsRep> FeedsRepClients = new ClientFactory<>(Feeds.SERVICENAME, RestFeedsPullClientRep::new, null) ;
+
 }
